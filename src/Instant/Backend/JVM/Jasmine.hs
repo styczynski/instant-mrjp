9c4d681e2907d14bf2826b1985bdd8e5ad4f3f93
styczynski <piotr@styczynski.in>
2023-10-31 16:38:54 +0100

    refactor

--- src/Instant/Backend/JVM/Jasmine.hs
+++ src/Instant/Backend/JVM/Jasmine.hs
@@ -18,29 +18,21 @@ import qualified Language.JVM.Common as J
 
 serializeOp :: J.Instruction -> String
 serializeOp = \case
-  J.Ldc (J.Integer (-1)) -> "iconst_m1"
-  J.Ldc (J.Integer n) | n <= 5 -> "iconst_" ++ show n
-  J.Ldc (J.Integer n) -> "bipush " ++ show n
-
-  J.Istore n | n < 0 -> error $ "Bad store num: " <> show n
-  J.Istore n  | n <= 3 -> "istore_" ++ show n
-  J.Istore n  -> "istore " ++ show n
-
-  J.Iload n | n < 0 -> error $ "Bad load num: " <> show n
-  J.Iload n | n <= 3 -> "iload_" ++ show n
-  J.Iload n -> "iload " ++ show n
-
   J.Iadd -> "iadd"
+  J.Getstatic fieldId -> "getstatic " ++ (J.unClassName $ J.fieldIdClass fieldId) ++ " " ++ (J.fieldIdName fieldId)
+  J.Invokevirtual _ m -> "invokevirtual " ++ (J.methodKeyName m)
   J.Isub -> "isub"
   J.Imul -> "imul"
   J.Idiv -> "idiv"
-
-  -- J.Swap -> "swap"
-
-  J.Getstatic fieldId -> "getstatic " ++ (J.unClassName $ J.fieldIdClass fieldId) ++ " " ++ (J.fieldIdName fieldId)
-  -- GETSTATIC t c -> "getstatic " ++ t ++ " " ++ c
-  J.Invokevirtual _ m -> "invokevirtual " ++ (J.methodKeyName m)
-
+  J.Ldc (J.Integer (-1)) -> "iconst_m1"
+  J.Ldc (J.Integer n) | n <= 5 -> "iconst_" ++ show n
+  J.Ldc (J.Integer n) -> "bipush " ++ show n
+  J.Iload n | n < 0 -> error $ "Bad load num: " <> show n
+  J.Iload n | n <= 3 -> "iload_" ++ show n
+  J.Iload n -> "iload " ++ show n
+  J.Istore n | n < 0 -> error $ "Bad store num: " <> show n
+  J.Istore n  | n <= 3 -> "istore_" ++ show n
+  J.Istore n  -> "istore " ++ show n
 
 intToWord16 :: Int -> Word16
 intToWord16 n = fromIntegral n
